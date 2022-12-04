dab07b4388cc0bf3aad4069ad2512359b58ee1cc
Jiri Suchan <yed@vanyli.net>
2022-12-04 13:23:42 +0700

    day4: python

--- python/day4.py
+++ python/day4.py
@@ -1,11 +1,69 @@
+import re
+
+
 def p1(data):
-    pass
+    overlaps = 0
+    re_match = re.compile(r"(\d+)\-(\d+),(\d+)\-(\d+)")
+
+    for line in data:
+        if not line:
+            continue
+        d1_from, d1_to, d2_from, d2_to = [int(x) for x in re_match.match(line).groups()]
+        if d1_from <= d2_from and d1_to >= d2_to:
+            overlaps += 1
+
+        elif d1_from >= d2_from and d1_to <= d2_to:  # d1 is subset
+            overlaps += 1
+
+    return overlaps
 
 
 def p2(data):
-    pass
+    overlaps = 0
+    re_match = re.compile(r"(\d+)\-(\d+),(\d+)\-(\d+)")
+
+    for line in data:
+        if not line:
+            continue
+        d1_from, d1_to, d2_from, d2_to = [int(x) for x in re_match.match(line).groups()]
+
+        if d1_from <= d2_from and d1_to >= d2_from:
+            overlaps += 1
+
+        elif d1_from >= d2_from and d1_from <= d2_to:
+            overlaps += 1
+
+    return overlaps
 
 
 def run(data):
     print(p1(data))
     print(p2(data))
+
+
+def test_p1():
+    data_raw = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+    data_lines = [x.strip() for x in data_raw.split()]
+
+    res = p1(data_lines)
+    assert res == 2
+
+
+def test_p2():
+    data_raw = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+    data_lines = [x.strip() for x in data_raw.split()]
+
+    res = p2(data_lines)
+    assert res == 4
--- python/day_template.py
+++ python/day_template.py
@@ -9,3 +9,23 @@ def p2(data):
 def run(data):
     print(p1(data))
     print(p2(data))
+
+
+def test_p1():
+    data_raw = """
+    """
+
+    data_lines = [x.strip() for x in data_raw.split()]
+
+    res = p1(data_lines)
+    assert res == None
+
+
+def test_p2():
+    data_raw = """
+    """
+
+    data_lines = [x.strip() for x in data_raw.split()]
+
+    res = p2(data_lines)
+    assert res == None
