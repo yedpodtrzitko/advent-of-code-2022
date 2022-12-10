17aa29595c3da205cc69365bee2a0b0fd931bceb
Jiri Suchan <yed@vanyli.net>
2022-12-10 13:43:45 +0700

    day 10: python

---
+++ python/day10.py
@@ -0,0 +1,378 @@
+def p1(data):
+    cycle = 1
+    sums = 0
+    X = 1
+
+    for line in data:
+        if line == "noop":
+            cycles = 1
+            add_value = 0
+        else:
+            cycles = 2
+            _, count_str = line.split()
+            add_value = int(count_str)
+
+        for _ in range(cycles):
+            if not (cycle + 20) % 40 or cycle == 20:
+                sums += X * cycle
+            cycle += 1
+        else:
+            X += add_value
+
+    return sums
+
+
+def p2(data):
+    cycle = 1
+    X = 1
+    render = [["."] * 40 for _ in range(6)]
+
+    for line in data:
+        if line == "noop":
+            cycles = 1
+            add_value = 0
+        else:
+            cycles = 2
+            _, count_str = line.split()
+            add_value = int(count_str)
+
+        for _ in range(cycles):
+            render_row = (cycle - 1) // 40
+            render_col = (cycle - 1) % 40
+
+            if X in [
+                render_col - 1,
+                render_col,
+                render_col + 1,
+            ]:
+                render[render_row][render_col] = "#"
+            cycle += 1
+
+        else:
+            X += add_value
+
+    render_str = "\n".join(["".join(x) for x in render])
+    return render_str
+
+
+def run(data):
+    print(p1(data))
+    print(p2(data))
+
+
+def test_p1():
+    data_expected = 13140
+
+    data_raw = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+    data_lines = [x for x in data_raw.split("\n")][1:]
+    res = p1(data_lines)
+    assert res == data_expected
+
+
+def test_p2():
+    data_expected = """
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
+
+    data_raw = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+    data_lines = [x for x in data_raw.split("\n")][1:]
+    res = p2(data_lines)
+    assert res == data_expected[1:]
